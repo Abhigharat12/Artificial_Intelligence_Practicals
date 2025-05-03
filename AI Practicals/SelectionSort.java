ce5af7563f08952d2615bfbf4d929cb780115984
Abhishek Gharat <156587972+Abhigharat12@users.noreply.github.com>
2025-05-03 15:41:28 +0530

    Update SelectionSort.java
    reduced complexity

--- AI Practicals/SelectionSort.java
+++ AI Practicals/SelectionSort.java
@@ -1,33 +1,29 @@
 public class SelectionSort {
-    public static void selectionSort(int[] array) {
-        int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
-            }
-           
-            int temp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = temp;
+          
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
     }
 
-    public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
+    public static void printArray(int[] arr) {
+        for (int num : arr) System.out.print(num + " ");
         System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
         System.out.println("Original array:");
-        printArray(array);
-        selectionSort(array);
+        printArray(arr);
+        selectionSort(arr);
         System.out.println("Sorted array:");
-        printArray(array);
+        printArray(arr);
     }
-} 
+}
